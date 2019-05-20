535ab627a6eaac5577257530abcd083a7bc331b0
Jing <jingkcheng@hotmail.com>
2019-05-20 02:40:35 -0400

    MemberEdit model changes as well as  member edit view changes

--- Trident/Models/ViewModels/MemberEdit.cs
+++ Trident/Models/ViewModels/MemberEdit.cs
@@ -16,6 +16,7 @@ namespace Trident.Models.ViewModels
         //To edit a member need a list of members
         public virtual Member member { get; set; }
 
-
+        //Need info about different teams this member could be assigned to
+        public IEnumerable<Team> teams { get; set; }
     }
 }
