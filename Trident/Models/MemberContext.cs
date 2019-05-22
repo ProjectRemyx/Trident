226acfbb5860226ee1d810f397111f98926d0c1e
Jing <jingkcheng@hotmail.com>
2019-05-22 16:38:25 -0400

    Changes in Web.config as well as DbContext to switch to MySQL compared to MSSQL

--- Trident/Models/MemberContext.cs
+++ Trident/Models/MemberContext.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using MySql.Data.Entity;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
 
 namespace Trident.Models
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class MemberContext : DbContext
     {
-        //Empty Constructor
-        public MemberContext()
+        //Constructor
+        public MemberContext() : base("Server=192.185.6.31; Database=lilxprince_trident; Uid=lilxp_jing; Pwd=h70bnrw8wvc")
         {
-            
+
         }
 
         public DbSet<Member> Members { get; set; }
@@ -19,12 +23,12 @@ namespace Trident.Models
 
         public DbSet<Team> Teams { get; set; }
 
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             //https://stackoverflow.com/questions/3600175/the-model-backing-the-database-context-has-changed-since-the-database-was-crea
             Database.SetInitializer<MemberContext>(null);
             base.OnModelCreating(modelBuilder);
         }
-        
     }
 }
